e3c04a00261d45fed26d18c767de1badb5073867
Thyago-ES <thyagorafael57@gmail.com>
2024-12-23 14:40:30 -0300

    feat: create initial structure of Express server

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import router from "./routes";
+
+import multer from "multer";
+import { storage } from "./config/multer";
+
+const upload = multer({ storage });
+
+class App {
+	server: express.Application;
+
+	constructor() {
+		this.server = express();
+		this.middlewares();
+		this.routes();
+	}
+
+	middlewares() {
+		this.server.use(express.json());
+	}
+
+	routes() {
+		this.server.use("/uploads", upload.single("file"), router);
+	}
+}
+
+export default new App().server;
